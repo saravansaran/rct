b43afdb28174b4ee4103f25b0cd521a2ade479d4
Saravan Kumar <saravankumar.b@novactech.in>
2024-05-30 16:35:57 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Primarybutton from "./components/button/Primarybutton";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>Test App</h3>
+      <Primarybutton></Primarybutton>
     </div>
   );
 }
