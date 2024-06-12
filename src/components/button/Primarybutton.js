0bd9a8f30d5c8174b70c9d807e7042d467b04851
Saravan Kumar <saravankumar.b@novactech.in>
2024-06-12 18:41:39 +0530

    added list component

--- src/components/button/Primarybutton.js
+++ src/components/button/Primarybutton.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./Primarybutton.css";
 
-export default function Primarybutton() {
+export default function Primarybutton(props) {
+  console.log(props);
   return (
     <>
-      <button className="primary-btn">Primary Button</button>
+      <button className="primary-btn">{props.buttonName}</button>
     </>
   );
 }
